31fe3af11828029441fd702ec850933a081223be
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-06-14 10:50:55 +0700

    :beers:

--- eye_detection_demo.py
+++ eye_detection_demo.py
@@ -4,7 +4,9 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("detectors/haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier("detectors/haarcascade_eye.xml")
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
+
+i = 0
 
 while True:
     ret, img = cap.read()
@@ -12,13 +14,18 @@ while True:
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     eyes = eye_cascade.detectMultiScale(gray)
+    eyeimages = []
     for (ex, ey, ew, eh) in eyes:
-        cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+        eye = img[ey : ey + eh, ex : ex + ew]
+        eyeimages.append(eye)
+        cv2.imwrite("eyes/{}.png".format(i), eye)
+        i += 1
 
-    cv2.imshow("img", img)
-    k = cv2.waitKey(30) & 0xFF
-    if k == 27:
-        break
+    if eyeimages:
+        cv2.imshow("img", eyeimages[0])
+        k = cv2.waitKey(30) & 0xFF
+        if k == 27:
+            break
 
 cap.release()
 cv2.destroyAllWindows()

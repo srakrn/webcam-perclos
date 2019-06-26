d524da701e26319905a3c5195d72fddfdebb944c
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-06-26 19:50:11 +0700

    A lot has changed, like love.

--- eye_detection_demo.py
+++ eye_detection_demo.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import norm
 import cv2
 import dlib
 
@@ -6,25 +7,45 @@ face_cascade = cv2.CascadeClassifier("detectors/haarcascade_frontalface_default.
 eye_cascade = cv2.CascadeClassifier("detectors/haarcascade_eye.xml")
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("eyes/videos/1.mp4")
+cap = cv2.VideoCapture("eyes/videos/dslr_fullface_hd_60fps.mp4")
 
-i = 0
+
+def eye_ear_score(eye_landmarks):
+    eye_landmarks = np.array(eye_landmarks)
+    dh1 = eye_landmarks[1] - eye_landmarks[5]
+    dh2 = eye_landmarks[2] - eye_landmarks[4]
+    dv = eye_landmarks[0] - eye_landmarks[3]
+    return (norm(dh1) + norm(dh2))/(2*norm(dv))
+
 
 while True:
     ret, img = cap.read()
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = detector(grayscale)
-    for face in faces:
-        l, t, r, b = face.left(), face.top(), face.right(), face.buttom()
-        cv2.rectangle(img, (l, t), (r, b), (255, 0 , 0), 5)
+    for face in faces[:1]:
+        l, t, r, b = face.left(), face.top(), face.right(), face.bottom()
+        cv2.rectangle(img, (l, t), (r, b), (255, 0, 0), 5)
         landmarks = predictor(grayscale, face)
-        for i in range(68):
-            x, y = landmarks.part(n).x, landmarks.part(n).y
-            cv2.circle(frame, (x, y), 4, (0, 255, 0), -1)
-    
+        left_eyes = []
+        right_eyes = []
+        for i in range(36, 42):
+            x, y = landmarks.part(i).x, landmarks.part(i).y
+            left_eyes.append([x, y])
+        for i in range(42, 48):
+            x, y = landmarks.part(i).x, landmarks.part(i).y
+            right_eyes.append([x, y])
+        print(left_eyes)
+        print(right_eyes)
+        for (x, y) in left_eyes:
+            cv2.circle(img, (x, y), 2, (0, 255, 0), -1)
+        for (x, y) in right_eyes:
+            cv2.circle(img, (x, y), 2, (0, 0, 255), -1)
+        left_ear, right_ear = eye_ear_score(left_eyes), eye_ear_score(right_eyes)
+        print("{} {}".format(left_ear, right_ear))
     cv2.imshow("Frame", img)
+    input()
+
     key = cv2.waitKey(1)
     if key == 27:
         break
